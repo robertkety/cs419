b11e5e8c24812145957cd16e6b696c5ced72c6ce
Michael Camilleri <mike@mikecamilleri.com>
2015-04-19 21:04:40 -0700

    another attempt to fix the migration issue

--- django_app/settings.py
+++ django_app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reuse_directory',
+    'corvallis_reuse',
 )
 
 MIDDLEWARE_CLASSES = (
