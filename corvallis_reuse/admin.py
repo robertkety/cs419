4b45b9b6635422c86474e321d0932ef3b3ac30f8
Michael Camilleri <mike@mikecamilleri.com>
2015-04-19 21:28:57 -0700

    added basic admin functionality for each model

--- corvallis_reuse/admin.py
+++ corvallis_reuse/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import organization, item, repairable, reusable, category, item_category
+
+admin.site.register(organization)
+admin.site.register(item)
+admin.site.register(repairable)
+admin.site.register(reusable)
+admin.site.register(category)
+admin.site.register(item_category)
