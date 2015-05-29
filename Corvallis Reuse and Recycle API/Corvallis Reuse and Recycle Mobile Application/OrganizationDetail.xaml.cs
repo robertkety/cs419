dded42192d2a906d89ca8a2b0b00c28064e8069a
Robert Kety <robertkety@gmail.com>
2015-05-28 22:42:28 -0700

    Corrected URLs in database and added hyperlinks
    URLs in the detailed organization view are now hyperlinks.  We should
    add valid URL confirmation in the Web Management Portal

--- Corvallis Reuse and Recycle API/Corvallis Reuse and Recycle Mobile Application/OrganizationDetail.xaml.cs
+++ Corvallis Reuse and Recycle API/Corvallis Reuse and Recycle Mobile Application/OrganizationDetail.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Reflection;
+using Windows.UI.Xaml.Documents;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
@@ -39,7 +40,9 @@ namespace Corvallis_Reuse_and_Recycle_Mobile_Application
             string OrganizationId = e.Parameter.ToString();
             Organization result = await DataAccess.GetOrganization(OrganizationId);
             List<string> OrgDetails = new List<string>();
-            
+            Uri uriResult;
+            HyperlinkButton hyperlinkBlock = new HyperlinkButton();
+
             OrgDetails.Add(result.Name);
 
             if (result.AddressLine1 != "") OrgDetails.Add(result.AddressLine1);
@@ -48,9 +51,23 @@ namespace Corvallis_Reuse_and_Recycle_Mobile_Application
             if (result.ZipCode != "") OrgDetails.Add(await DataAccess.GetCityState(result.ZipCode));
 
             if (result.Phone != "") OrgDetails.Add(String.Format("({0}) {1}-{2}", result.Phone.Substring(0, 3), result.Phone.Substring(3, 3), result.Phone.Substring(6)));
-            if (result.Website != "") OrgDetails.Add(result.Website);
             if (result.Hours != "") OrgDetails.Add(result.Hours);
             if (result.Notes != "") OrgDetails.Add(result.Notes);
+            if (result.Website != "")
+            {
+                if (Uri.TryCreate(result.Website, UriKind.Absolute, out uriResult))
+                {
+                    hyperlinkBlock.FontSize = 24;
+                    hyperlinkBlock.Margin = new Thickness(50, 0, 10, 10);
+                    hyperlinkBlock.HorizontalAlignment = HorizontalAlignment.Left;
+                    hyperlinkBlock.VerticalAlignment = VerticalAlignment.Center;
+                    hyperlinkBlock.Content = result.Website;
+                    hyperlinkBlock.NavigateUri = uriResult;
+                    hyperlinkBlock.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 80, 119, 39));
+                }
+                else
+                    OrgDetails.Add(result.Website);
+            }
 
             foreach (string detail in OrgDetails)
             {
@@ -64,6 +81,10 @@ namespace Corvallis_Reuse_and_Recycle_Mobile_Application
                 textBlock.Foreground = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 80, 119, 39));
                 OrganizationDetails.Children.Add(textBlock);
             }
+
+            if (hyperlinkBlock != null)
+                OrganizationDetails.Children.Add(hyperlinkBlock);
+
         }
     }
 }
